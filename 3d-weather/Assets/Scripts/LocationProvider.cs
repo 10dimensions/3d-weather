64a0106dc5d169069f22adb01b52c6686ba4db0f
10dimensions <relay.10d@gmail.com>
2019-02-08 18:03:33 +0530

    _location

---
+++ 3d-weather/Assets/Scripts/LocationProvider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationProvider : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetLocation());
+    }
+
+    IEnumerator GetLocation()
+    {
+        if (!Input.location.isEnabledByUser)
+            {   
+                Debug.Log("disabled");
+                yield break;
+            }
+
+       Input.location.Start();
+
+       int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+            Debug.Log(maxWait);
+        }
+
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        Input.location.Stop();
+    }
+}
