fd6dcaa2bd21d2735910b3d65a30d5b814928a1e
10dimensions <relay.10d@gmail.com>
2019-02-08 19:22:27 +0530

    _deserialize_json

--- 3d-weather/Assets/Scripts/Singleton.cs
+++ 3d-weather/Assets/Scripts/Singleton.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Singleton : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    [SerializeField] private string url;
+    [SerializeField] private float lat;
+    [SerializeField] private float lon;
+
+    public OpenWeather WeatherData;
+
+     void Start()
+    {
+        WeatherData = new OpenWeather();
+        StartCoroutine(OpenWeatherAPI(url));
+    }
+ void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CallOpenWeather()
     {
-        
+
     }
+
+   IEnumerator OpenWeatherAPI(string url)
+{
+
+    UnityWebRequest www = UnityWebRequest.Get(url);
+    www.SetRequestHeader("Cache-Control", "max-age=0, no-cache, no-store");
+    www.SetRequestHeader("Pragma", "no-cache");
+
+    yield return www.SendWebRequest();
+    if (www.isNetworkError)
+    {
+        Debug.Log(www.error);
+    }
+    else
+    {
+        Debug.Log("Received " + www.downloadHandler.text);
+        WeatherData = JsonUtility.FromJson<OpenWeather>(www.downloadHandler.text);
+    }
+}
+ 
 }
